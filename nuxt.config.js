1584c76508b55736cbf218f764d343121eb530fb
gutonteen <gutonteen@gmail.com>
2022-11-17 22:40:51 +0700

    target server

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -39,4 +39,5 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+  target: 'server',
 }
