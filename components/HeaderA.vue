27e0011b63a2297d0a1fa9486c177402281f32b6
gutonteen <gutonteen@gmail.com>
2022-11-25 12:22:51 +0700

    update template

---
+++ components/HeaderA.vue
@@ -0,0 +1,47 @@
+<!-- Please remove this file from your project -->
+<template>
+  <nav
+    id="mainNav"
+    class="navbar navbar-expand-lg navbar-light fixed-top bg-primary"
+  >
+    <div class="container px-4 px-lg-5">
+      <a class="navbar-brand" href="#page-top"
+        ><img width="200" src="Resili_WG_logo.png"
+      /></a>
+      <button
+        class="navbar-toggler navbar-toggler-right"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarResponsive"
+        aria-controls="navbarResponsive"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div id="navbarResponsive" class="collapse navbar-collapse">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#ebook">Ebook</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#projects">Projects</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#signup">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'HeaderA',
+}
+</script>
